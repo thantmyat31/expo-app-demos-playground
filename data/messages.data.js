f25ba62fece8a552007391e376887ecf88f70695
thantmyat <thantmyat1234@gmail.com>
2020-04-13 17:05:29 +0630

    update send message screen

--- data/messages.data.js
+++ data/messages.data.js
@@ -2,7 +2,15 @@ const MESSAGES_DATA = [
 	{
 		id: '1',
 		typeId: '1',
-		title: 'Message title',
+		title: 'Message title 1',
+		imageUrl: 'https://www.aspiremag.net/inspire/wp-content/uploads/2014/08/Be-Happy.jpg',
+		content:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+	},
+	{
+		id: '2',
+		typeId: '2',
+		title: 'Message title 2',
 		imageUrl: 'https://www.aspiremag.net/inspire/wp-content/uploads/2014/08/Be-Happy.jpg',
 		content:
 			'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
--- screens/messages/sendMessage.screen.js
+++ screens/messages/sendMessage.screen.js
@@ -1,28 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, CheckBox } from 'react-native';
 
 import Color from './../../constants/colors.constant';
+import { TextInput } from 'react-native-gesture-handler';
 
 const SendMessageScreen = () => {
 	const [ dateTime, setDateTime ] = useState();
+	const [ isSelected, setSelection ] = useState(false);
+
 	const setDateAndTime = () => {
 		const newDate = new Date();
 		const date = newDate.toLocaleDateString();
 		const time = newDate.toLocaleTimeString();
-		setDateTime(`${date} ${time}`);
+		setDateTime(`${date}`);
 	};
 
 	useEffect(() => {
 		setDateAndTime();
 	}, []);
 
-	// setInterval(setDateAndTime, 1000);
-
 	return (
 		<View style={styles.screen}>
 			<View style={styles.dateTime}>
 				<Text>Note date</Text>
 				<Text>{dateTime}</Text>
+			</View>
+			<View style={styles.options}>
+				<View style={styles.option}>
+					<CheckBox value={isSelected} onValueChange={setSelection} style={styles.checkbox} />
+					<Text style={styles.optionText}>Information</Text>
+				</View>
+				<View style={styles.option}>
+					<CheckBox value={isSelected} onValueChange={setSelection} style={styles.checkbox} />
+					<Text style={styles.optionText}>Suggestion</Text>
+				</View>
+			</View>
+			<View style={styles.details}>
+				<Text style={styles.label}>Messages</Text>
+				<TextInput style={styles.input} placeholder="messages" />
 			</View>
 		</View>
 	);
@@ -40,7 +55,33 @@ const styles = StyleSheet.create({
 		paddingHorizontal: 20,
 		paddingVertical: 30,
 		borderBottomWidth: 1,
-		borderBottomColor: '#ccc'
+		borderBottomColor: '#ccc',
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	},
+	options: {
+		paddingHorizontal: 20,
+		paddingVertical: 10,
+		borderBottomColor: '#ccc',
+		borderBottomWidth: 1
+	},
+	option: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		paddingVertical: 5
+	},
+	optionText: {
+		fontSize: 16,
+		paddingLeft: 20
+	},
+	details: {
+		paddingHorizontal: 20,
+		paddingVertical: 20
+	},
+	input: {
+		borderBottomWidth: 1,
+		borderBottomColor: '#ccc',
+		paddingVertical: 10
 	}
 });
 
